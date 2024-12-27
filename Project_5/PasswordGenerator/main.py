9992a6642086273e129c636855fc7de8b0789179
Hassan Ahmed <hassanahmed40005@gmail.com>
2024-12-27 10:21:00 +0500

    Project 05 of Day 5

--- Project_5/PasswordGenerator/main.py
+++ Project_5/PasswordGenerator/main.py
@@ -17,28 +17,22 @@ symbol_list=[]
 for letter in range(letter_in):
     random_no=random.choice(letters)
     letter_list.append(random_no)
-# print(letter_list)
 
 for no in range(numbers_in):
     random_no=random.choice(numbers)
     number_list.append(random_no)
-# print(number_list)
+
 
 for special in range(symbols_in):
     random_no=random.choice(symbols)
     symbol_list.append(random_no)
-# print(symbol_list)
 
 combine_randomeness= letter_list+number_list+symbol_list
-# print(combine_randomeness)
 final_list=[]
 for randomess in combine_randomeness:
     final_no=random.choice(combine_randomeness)
     final_list.append(final_no)
-# print(final_list)
 
 print("Your password is", end=' ')
-# for i in range(len(final_list)):
-#     print(f"Your password is {final_list[i]}",end=' ')
 for password in final_list:
     print(f"{password}",end=' ')
